79413563b536c2b05a1a45e248e30bcd4a374af3
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-06-18 16:38:03 +0300

    Initial Commit

---
+++ MapKitUI/MapKitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitUIApp.swift
+//  MapKitUI
+//
+//  Created by Amby on 18/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapKitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
